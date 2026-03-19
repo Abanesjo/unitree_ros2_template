abfcb9a0993cbc7f53f22fdd3056d6bfe629d5c1
john.abanes@nyu.edu <john.abanes@nyu.edu>
2026-03-19 18:34:39 -0400

    change docker image

--- build_and_run.sh
+++ build_and_run.sh
@@ -6,4 +6,5 @@ docker run -it --rm --name unitree_ros2 --network host \
  -e DISPLAY=$DISPLAY \
  -e NVIDIA_DRIVER_CAPABILITIES=all \
  -v /tmp/.X11-unix:/tmp/.X11-unix \
+ -v "$(dirname "$(readlink -f "$0")"):/workspace/src/unitree_ros2" \
  unitree_ros2:v1
