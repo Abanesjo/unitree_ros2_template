c0a314497dbf786ef8a2e7d943cf30f0c63360fc
John Abanes <john.abanes@nyu.edu>
2026-03-29 21:40:27 -0400

    update cmake paths in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,5 +13,7 @@ source /workspace/install/setup.bash
 echo "source /workspace/install/setup.bash" >> ~/.bashrc
 echo "export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp" >> ~/.bashrc
 echo "export CYCLONEDDS_URI=file:///workspace/src/unitree_ros2/cyclonedds.xml" >> ~/.bashrc
+echo "export CMAKE_PREFIX_PATH=/opt/unitree_robotics:/opt/cyclonedds:\$CMAKE_PREFIX_PATH" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=/opt/unitree_robotics/lib:/opt/cyclonedds/lib:\$LD_LIBRARY_PATH" >> ~/.bashrc
 
 exec bash
